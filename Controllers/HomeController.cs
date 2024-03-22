568b73fd9d18e2b354bae077b2f92dd62e5fed26
OmkarMohite505 <omkar.mohite@nitorinfotech.net>
2024-03-22 19:16:34 +0530

    project created

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("need_success")]
+        public IActionResult SendSuccessMessage()
+        {
+            return Ok("Success message from Docker Image");
+        }
+    }
+}
